9ac67f6274a9686ebf07cf36fea789357ddb2991
parvejg <parvezpg321@gmail.com>
2023-11-23 19:29:18 +0530

    create search menu component and css file

---
+++ src/Components/Search-menu.js
@@ -0,0 +1,9 @@
+import "./Search-menu.css";
+export const SearchMenu = () => {
+  return (
+    <div className="search-menu-main-container">
+      <di className="side-bar-container">side bar </di>
+      <div className="search-menu-contents">page content</div>
+    </div>
+  );
+};
